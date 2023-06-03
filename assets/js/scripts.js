0f3ee2be05a74d16588c8753f060a1810954fa0f
Didier Morantes <emorantes@edutecno.com>
2023-06-03 12:21:27 -0500

    creacion del repositorio local

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+const a=()=>{
+    return new Promise(resolve =>{
+        setTimeout( ()=>{
+            resolve('Se resolvió la promesa a después de 3 segundos');
+        } , 3000);
+    });
+};
+
+const b=()=>{
+    return new Promise(resolve =>{
+        setTimeout( ()=>{
+            resolve('Se resolvió la promesa b después de 1 segundos');
+        } , 1000);
+    });
+};
+
+
+const llamadaAsincrona = async ()=>{
+    console.log('Iniciando llamadaAsincrona');
+    const resultado1 = await a();
+    console.log(resultado1);
+    const resultado2 = await b();
+    console.log(resultado2);
+    console.log('Fin llamadaAsincrona');
+};
+
+
+const asignarEventos = ()=>{
+    let elBoton = document.getElementById('btnAsync');
+    elBoton.addEventListener('click', llamadaAsincrona);
+};
